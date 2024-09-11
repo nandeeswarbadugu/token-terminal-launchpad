fe6533c2f910a602d34c283ced78219de3e2db2e
nandeeswarbadugu <169081821+nandeeswarbadugu@users.noreply.github.com>
2024-09-11 01:31:51 -0500

    added wallet adapter

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import { TokenLaunchpad } from './components/TokenLaunchpad'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui'
+import '@solana/wallet-adapter-react-ui/styles.css'
+
+
+function App() {
+  return (
+    <div style={{width:"100vw"}}>
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <WalletProvider wallets={[]}>
+        <WalletModalProvider>
+          <div style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            padding: 20
+          }}>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+          </div>
+          <TokenLaunchpad />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+    </div>
+  )
+}
+
+export default App
